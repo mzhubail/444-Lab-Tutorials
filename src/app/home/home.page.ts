56db67835ce986c5ad7f259844df3ba59789b2bf
mzhubail <mzhubail@gmail.com>
2023-11-09 19:05:20 +0300

    Q1C
    Implmeneted delete button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,23 @@ export class HomePage {
 
   constructor(
     public dataService : DataService,
+    public loadingCtrl : LoadingController,
   ) {}
 
   showHistory(index: number) {
     this.showHistoryIndex = this.showHistoryIndex == index ? -1 : index;
+  }
+
+  // async _deleteProduct(index: number) {
+  //   const loading = await this.loadingCtrl.create({
+  //     message: 'Deleting Product...',
+  //     duration: 2000,
+  //   });
+  //   await loading.present();
+  //   // loading.onDidDismiss = () => { this.dataService.list.splice(index, 1) };
+  // }
+
+  deleteProduct(index: number) {
+    this.dataService.list.splice(index, 1);
   }
 }
