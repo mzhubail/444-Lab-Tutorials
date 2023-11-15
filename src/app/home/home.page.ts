2cf462db0722e073ed8ef063e4d97fe4b856e1d4
mzhubail <mzhubail@gmail.com>
2023-11-15 11:36:39 +0300

    Q2A

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DataService } from '../data.service';
+import { DataService, RanVal } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -16,4 +16,37 @@ export class HomePage {
   toggleShowAdd() {
     this.showAdd = !this.showAdd;
   }
+
+  number = 0;
+  addNumber () {
+    var v : RanVal = {
+      Number: this.number,
+      Even: [],
+      Odd: [],
+      Prime: [],
+    };
+
+    for (let i = 0; i <= this.number; i++) {
+      if (i % 2 == 0) // is Even
+        v.Even.push(i);
+      else            // is Odd
+        v.Odd.push(i);
+      if (isPrime(i))
+        v.Prime.push(i);
+    }
+    console.log(v);
+    this.dataService.list.push(v);
+  }
+}
+
+
+function isPrime(num: number): boolean {
+  if (num == 0)
+    return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
