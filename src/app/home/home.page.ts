7b60198580c65f80bb25c1327b9f6165a1e3cb3a
mzhubail <mzhubail@gmail.com>
2023-12-03 16:31:48 +0300

    Working sign in and sign out

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  email = '';
+  password = '';
 
-  constructor() {}
+  constructor(
+    public authService: AuthService,
+  ) { }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Auth, User, onAuthStateChanged, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user!: User | null;
+
+  constructor(
+    // private f: FirebaseApp,
+    private auth: Auth,
+  ) {
+    // console.log(
+    //   auth.currentUser
+    // );
+    onAuthStateChanged(
+      auth,
+      user => {
+        // if (user)
+        //   this.user = user
+        // else
+        // this.user = null;
+        this.user = user;
+      }
+    );
+  }
+
+  signIn(email: string, password: string) {
+    // const auth = getAuth(this.f);
+    signInWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+        console.log(`Signed in ${user.email}`);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log({ errorCode, errorMessage });
+      });
+  }
+
+  signOut() {
+    signOut(this.auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
+}
