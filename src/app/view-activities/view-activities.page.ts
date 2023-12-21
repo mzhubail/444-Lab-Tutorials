fa3d2461b35fa57169026ce8fe772f8538766760
mzhubail <mzhubail@gmail.com>
2023-12-21 12:42:30 +0300

    6.5 -- Added search in view activities

--- src/app/view-activities/view-activities.page.ts
+++ src/app/view-activities/view-activities.page.ts
@@ -9,11 +9,16 @@ import { Activity, ActivityService } from '../services/activity.service';
 export class ViewActivitiesPage implements OnInit {
   activities!: Activity[];
 
+  // Seach related
+  filteredActivities!: Activity[];
+  searchTerm = '';
+
   constructor(
     public activityService: ActivityService,
   ) {
     activityService.activities$.subscribe(data => {
       this.activities = data;
+      this.filter();
     });
   }
 
@@ -23,5 +28,19 @@ export class ViewActivitiesPage implements OnInit {
   formatDate(ISOString: string) {
     const date = new Date(ISOString);
     return date.toDateString();
+  }
+
+  filter() {
+    if (this.searchTerm === '') {
+      this.filteredActivities = this.activities;
+      return;
+    }
+
+    this.filteredActivities =
+    this.activities.filter(
+      a => a.title.toLowerCase().includes(
+        this.searchTerm.toLowerCase()
+      )
+    )
   }
 }
