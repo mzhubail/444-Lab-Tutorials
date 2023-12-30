db07f848752e8cad5d62360ef18751928ef1ab87
mzhubail <mzhubail@gmail.com>
2023-12-30 14:37:47 +0300

    Added validation message utility

--- src/app/utilities.ts
+++ src/app/utilities.ts
@@ -1,3 +1,6 @@
+import { ValidationErrors } from "@angular/forms";
+
+
 /**
  * Filter a list.
  *
@@ -39,4 +42,41 @@ function match(word: string, searchTerm: string) {
   return word.toLowerCase().includes(
     searchTerm.toLowerCase()
   );
+}
+
+
+/** Converts angular provided validation errors to error message */
+export function convertErrorsToMessage(name: string, errors: ValidationErrors | null): string | undefined {
+  // console.log({name, errors: errors})
+  if (errors == null)
+    return;
+  var entries = Object.entries(errors);
+  if (entries.length == 0)
+    return;
+  var [errorName, errorContent] = entries[0];
+  // console.log(errorName, errorContent);
+
+  switch (errorName) {
+    case 'required':
+      return `${name} is required`;
+    case 'min':
+      return `${name} has to be greater than or equal to ${errorContent.min}`;
+    case 'max':
+      return `${name} has to be less than or equal to ${errorContent.max}`;
+    case 'minlength':
+      return `${name} has to be at least ${errorContent.requiredLength} characters`;
+    case 'maxlength':
+      return `${name} has to be at least ${errorContent.requiredLength} characters`;
+    case 'email':
+      return (name.toLowerCase() != 'email')
+        ? `${name} is not a valid email`
+        : `${name} is not valid`;
+    case 'pattern':
+      return `${name} is not valid`;
+
+    default:
+      console.log(errors);
+      console.warn(`The error '${errorName}' was not catched`, errorContent);
+      return `${name} is not valid`;
+  }
 }
