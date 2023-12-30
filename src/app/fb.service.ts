8f1e45b3259f70000fef99431970de7b55c9e568
mzhubail <mzhubail@gmail.com>
2023-12-30 17:32:00 +0300

    Q1C - Add device

--- src/app/fb.service.ts
+++ src/app/fb.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@angular/core';
-
+import {
+  CollectionReference,
+  Firestore,
+  collection,
+  collectionData,
+  doc,
+  setDoc,
+} from '@angular/fire/firestore';
 
 export interface Device {
-  serial: string,
-  model: string,
-  weight: string,
-  year: Date,
-};
+  serial: string;
+  model: string;
+  weight: string;
+  year: Date;
+}
 
 export interface History {
-  serial: string,
-  repairDate: Date,
-  cost: number,
-};
-
+  serial: string;
+  repairDate: Date;
+  cost: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FbService {
+  public devicesCollection;
 
-  constructor() { }
+  constructor(public db: Firestore) {
+    this.devicesCollection = collection(
+      db,
+      'Devices',
+    ) as CollectionReference<Device>;
+  }
+
+  addDevice(d: Device) {
+    const deviceDoc = doc(this.devicesCollection);
+    return setDoc(deviceDoc, d);
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Device } from '../fb.service';
+import { Device, FbService } from '../fb.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -9,8 +10,16 @@ import { Device } from '../fb.service';
 })
 export class Tab1Page {
   public form;
+  public props: Partial<Device> = {
+    weight: 'light',
+  };
+  public year: string = '';
 
-  constructor(formBuilder: FormBuilder) {
+  constructor(
+    formBuilder: FormBuilder,
+    public fbService: FbService,
+    public alertController: AlertController,
+  ) {
     this.form = formBuilder.group({
       model: [
         '',
@@ -21,5 +30,39 @@ export class Tab1Page {
         ],
       ],
     });
+  }
+
+  addDevice() {
+    const serial = this.props.serial,
+      weight = this.props.weight,
+      model = this.form.value.model;
+    if (!serial || !weight || !model) return;
+
+    const year = new Date(this.year);
+    this.fbService
+      .addDevice({
+        model: model,
+        serial: serial,
+        weight: weight,
+        year: year,
+      })
+      .then(() => {
+        this.alertController
+          .create({
+            header: 'Device added successfully',
+            buttons: ['OK'],
+          })
+          .then((a) => a.present());
+      })
+      .catch(() => {
+        this.alertController
+          .create({
+            header: 'Error',
+            message: 'Sorry.  We faced a problem adding your device.' +
+              'please try again later.',
+            buttons: ['Cancel'],
+          })
+          .then((a) => a.present());
+      });
   }
 }
