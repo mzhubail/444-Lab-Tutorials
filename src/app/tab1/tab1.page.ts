32fc6fa7693840d578a92858d6332e17b1651c29
mzhubail <mzhubail@gmail.com>
2023-12-30 17:13:06 +0300

    Q2B - Add form validation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Device } from '../fb.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  public form;
 
-  constructor() {}
-
+  constructor(formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      model: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern(/^[a-z0-9]*$/i),
+        ],
+      ],
+    });
+  }
 }
