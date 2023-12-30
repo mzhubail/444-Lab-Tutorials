aab182ab0a9b0ee2a92ce2cb745e07cabb65d5b8
mzhubail <mzhubail@gmail.com>
2023-12-30 23:10:34 +0300

    Q2G - Added `submit`

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FBService, User } from '../fb.service';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -18,7 +19,11 @@ export class Tab1Page {
   verifyMessage = '';
   verifyColor = '';
 
-  constructor(public serv: FBService, public formBuilder: FormBuilder) {
+  constructor(
+    public serv: FBService,
+    public formBuilder: FormBuilder,
+    public alertController: AlertController,
+  ) {
     this.form = this.formBuilder.group({
       name: [
         '',
@@ -55,5 +60,49 @@ export class Tab1Page {
     };
 
     this.serv.addUser(user);
+  }
+
+  submit() {
+    const { approved, quantity, shift } = this.props;
+    const { name } = this.form.value;
+
+    if (this.form.invalid) return;
+
+    const user: User = {
+      approved: approved,
+      name: name as string,
+      quantity: quantity,
+      shift: shift,
+      status: 'Pending',
+    };
+
+    this.serv
+      .addUser(user)
+      .then((x) => {
+        this.successAlert(x.id);
+      })
+      .catch(() => {
+        this.failureAlert();
+      });
+  }
+
+  async successAlert(id: string) {
+    const a = await this.alertController.create({
+      header: 'Success',
+      message: `User was inserted succesffuly.  ${id}`,
+      buttons: ['OK'],
+    });
+    a.present();
+  }
+
+  async failureAlert() {
+    const a = await this.alertController.create({
+      header: 'Error',
+      message:
+        'We faced some issues while trying to insert your user. ' +
+        'Please try again later.',
+      buttons: ['OK'],
+    });
+    a.present();
   }
 }
