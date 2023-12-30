110a7e33530da67d197287e74f49f954762b6a6b
mzhubail <mzhubail@gmail.com>
2023-12-30 22:17:51 +0300

    Q2A - Scaffold an interface

--- src/app/fb.service.ts
+++ src/app/fb.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+export interface User {
+  name: string;
+  shift: boolean[];
+  quantity: number;
+  status: string;
+  approved: boolean;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FBService {
+  public shifts = {
+    morning: 'Morning Shift',
+    afternoon: 'Afternoon Shift',
+    night: 'Night Shift',
+  };
 
-  constructor() { }
+  constructor() {}
 }
