0f1962f0da894e287c7409efaee5437aad0d07f3
mzhubail <mzhubail@gmail.com>
2023-12-30 23:58:11 +0300

    Q3C - Loading and animation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FBService, User } from '../fb.service';
+import { LoadingController } from '@ionic/angular';
+
+declare var dynamics: any;
 
 @Component({
   selector: 'app-tab2',
@@ -10,7 +13,10 @@ export class Tab2Page {
   users!: User[];
   showUser!: boolean[];
 
-  constructor(public serv: FBService) {
+  constructor(
+    public serv: FBService,
+    public LoadingController: LoadingController,
+  ) {
     serv.users$.subscribe((users) => {
       this.users = users;
 
@@ -30,4 +36,31 @@ export class Tab2Page {
     });
     return out;
   };
+
+  async animate(e: EventTarget | null) {
+    const loading = await this.LoadingController.create({
+      message: 'Please wait',
+    });
+    loading.present();
+
+    // setTimeout(() => {
+    //   loading.dismiss();
+    // }, 1500);
+
+    if (!e) return;
+    dynamics.animate(
+      e,
+      {
+        translateY: 0,
+        translateX: 100,
+      },
+      {
+        type: dynamics.linear,
+        duration: 1500,
+        complete: () => {
+          loading.dismiss();
+        },
+      },
+    );
+  }
 }
