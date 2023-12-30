1fb058db29f4f0c7cffd63b4bffd1c0f8ed84ea2
mzhubail <mzhubail@gmail.com>
2023-12-30 23:37:38 +0300

    Q3B - Implement detail button

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,12 +8,26 @@ import { FBService, User } from '../fb.service';
 })
 export class Tab2Page {
   users!: User[];
+  showUser!: boolean[];
 
   constructor(public serv: FBService) {
     serv.users$.subscribe((users) => {
       this.users = users;
+
+      this.showUser = [];
+      for (let i = 0; i < users.length; i++) {
+        this.showUser.push(false);
+      }
     });
   }
 
   formatName = (name: string) => (name.length === 0 ? 'NA' : name);
+
+  countShifts = (u: User) => {
+    const out: number[] = [];
+    u.shift.forEach((s) => {
+      if (s) out.push(1);
+    });
+    return out;
+  };
 }
