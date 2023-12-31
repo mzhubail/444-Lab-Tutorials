98347c0bd35cb2fe1740a07e858fbefe29d82046
mzhubail <mzhubail@gmail.com>
2023-12-31 10:20:52 +0300

    Q1A - Scaffold interface for tab1

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,5 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class DataService {
+  PAPERSIZES = ['A3', 'A4', 'A5', 'B5', 'letter'];
+  INKQUALITY = ['low', 'medium', 'high'];
+
   constructor() {}
 }
