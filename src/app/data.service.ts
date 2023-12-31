3e8b8e652c2feb5d9e41920aa7cff04d23d898a7
mzhubail <mzhubail@gmail.com>
2023-12-31 11:03:29 +0300

    Q2A - Scaffold interface with functional retrieval from database

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,9 +4,12 @@ import {
   Firestore,
   addDoc,
   collection,
+  collectionData,
 } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 export interface PrintingRequest {
+  id?: string;
   copiesCount: number;
   paperSize: 'A3' | 'A4' | 'A5' | 'B5' | 'letter';
   inkQuality: 'low' | 'medium' | 'hight';
@@ -22,12 +25,14 @@ export class DataService {
   INKQUALITY = ['low', 'medium', 'high'];
 
   reqRef;
+  requests$: Observable<PrintingRequest[]>;
 
   constructor(db: Firestore) {
     this.reqRef = collection(
       db,
       'Printing Requests',
     ) as CollectionReference<PrintingRequest>;
+    this.requests$ = collectionData(this.reqRef, { idField: 'id' });
   }
 
   addPriningRequest(req: PrintingRequest) {
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { DataService, PrintingRequest } from '../data.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  requests!: PrintingRequest[];
 
-  constructor() {}
-
+  constructor(public dataService: DataService) {
+    dataService.requests$.subscribe((requests) => {
+      this.requests = requests;
+    });
+  }
 }
