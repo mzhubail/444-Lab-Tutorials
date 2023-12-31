b2bbff68353a02fdf6259d837db9ea5d018a8d7a
mzhubail <mzhubail@gmail.com>
2023-12-31 10:33:24 +0300

    Q1B - Functional form

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 
+export interface PrintingRequest {
+  copiesCount: number;
+  paperSize: 'A3' | 'A4' | 'A5' | 'B5' | 'letter';
+  inkQuality: 'low' | 'medium' | 'hight';
+  hasPrintDate: boolean;
+  withBorders: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DataService } from '../data.service';
+import { DataService, PrintingRequest } from '../data.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,5 +7,19 @@ import { DataService } from '../data.service';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  props: PrintingRequest = {
+    copiesCount: 1,
+    inkQuality: 'low',
+    paperSize: 'letter',
+    hasPrintDate: false,
+    withBorders: false,
+  };
+
+  isCopiesCountValid = (): boolean =>
+    this.props.copiesCount > 0 && this.props.copiesCount <= 10;
+
   constructor(public dataService: DataService) {}
+
+  submit() {
+  }
 }
