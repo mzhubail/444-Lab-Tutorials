081bd52b46d04238bb483264cabe35f56241a181
mzhubail <mzhubail@gmail.com>
2023-12-31 10:46:34 +0300

    Q1D - Confirmation + alerts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService, PrintingRequest } from '../data.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -18,14 +19,65 @@ export class Tab1Page {
   isCopiesCountValid = (): boolean =>
     this.props.copiesCount > 0 && this.props.copiesCount <= 10;
 
-  constructor(public dataService: DataService) {}
+  constructor(
+    public dataService: DataService,
+    public alertController: AlertController,
+  ) {}
 
-  submit() {
+  async submit() {
     if (!this.isCopiesCountValid()) {
       console.error('Invalid');
       return;
     }
 
-    this.dataService.addPriningRequest(this.props);
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: 'Are you sure you want to send this request?',
+      buttons: [
+        'Cancel',
+        {
+          text: 'Confirm',
+          handler: () => {
+            this.sendRequest();
+          },
+        },
+      ],
+    });
+    alert.present();
+  }
+
+  sendRequest() {
+    this.dataService
+      .addPriningRequest(this.props)
+      .then(() => {
+        this.alertSuccess();
+      })
+      .catch(() => {
+        this.alertFailure();
+      });
+  }
+
+  alertSuccess() {
+    this.alertController
+      .create({
+        header: 'Success',
+        message: 'Your printing request was added successfully',
+        buttons: ['OK'],
+      })
+      .then((a) => {
+        a.present();
+      });
+  }
+
+  alertFailure() {
+    this.alertController
+      .create({
+        header: 'Error',
+        message: 'Could not add your request',
+        buttons: ['Cancel'],
+      })
+      .then((a) => {
+        a.present();
+      });
   }
 }
